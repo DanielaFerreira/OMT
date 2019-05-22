65af849e80f0e122b30a4ce4fafdfe8158823c62
eddunic <eduardo.bitencourt007@gmail.com>
2019-05-22 18:51:13 -0400

    formulario do egresso

---
+++ src/main/java/com/br/OMT/Servlets/EnviaFormEgresso.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.OMT.Servlets;
+
+import com.br.OMT.Utils.CommonsMail;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.mail.EmailException;
+
+/**
+ *
+ * @author eddunic
+ */
+public class EnviaFormEgresso extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException, MalformedURLException {
+	
+	try {
+	    CommonsMail.enviarEmail("eduardo.bitencourt007@gmail.com");
+	} catch (EmailException | MalformedURLException ex) {
+	    Logger.getLogger(EnviaFormEgresso.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	
+
+//	String acao = request.getParameter("acao");
+//	if(acao.equals("Enviar")){
+//    try {
+//	CommonsMail.enviarEmail(request.getParameter("email"));
+//    } catch (EmailException ex) {
+//	Logger.getLogger(EnviaFormEgresso.class.getName()).log(Level.SEVERE, null, ex);
+//    }
+//	}
+//	
+    }
+
+}
