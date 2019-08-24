e7ffeaed56197f38d512d3b5edefacd79b8972f3
eddunic <eduardo.bitencourt007@gmail.com>
2019-08-24 18:28:47 -0400

    Login com SIAPE/Usuário + alteração de Registro (com bug em acento)

--- src/main/java/com/br/OMT/Servlets/TrabalhoServlet.java
+++ src/main/java/com/br/OMT/Servlets/TrabalhoServlet.java
@@ -12,7 +12,8 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -36,10 +37,11 @@ public class TrabalhoServlet extends HttpServlet {
         String butao = request.getParameter("acao");
         Trabalho t = Trabalho.getInstance();
         TrabalhoDAO tdao = new TrabalhoDAO();
-        response.setCharacterEncoding("ISO-8859-1");
+        response.setCharacterEncoding("UTF-8");
         if (request != null) {
 
             if (butao.equals("cadastrar")) {
+                t.setEntidade((Entidade) request.getSession().getAttribute("entidade"));
                 t.setProfissao(request.getParameter("profissao"));
                 t.setTipo(request.getParameter("tipo").charAt(0));
                 t.setQuantidadeVagas(Integer.parseInt(request.getParameter("quantidadeVagas")));
@@ -48,9 +50,8 @@ public class TrabalhoServlet extends HttpServlet {
                 DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
                 df.setLenient(false);
                 try {
-                    t.setTempoFinal(df.parse(request.getParameter("inicio")));
-                    t.setTempoInicio(df.parse(request.getParameter("fim")));
-                    t.setEntidade((Entidade) request.getSession().getAttribute("entidade"));
+                    t.setTempoFinal(df.parse(request.getParameter("fim")));
+                    t.setTempoInicio(df.parse(request.getParameter("inicio")));
 
                     String str = tdao.salvar(t);
                     if (str.equals("")) {
@@ -68,8 +69,22 @@ public class TrabalhoServlet extends HttpServlet {
 
             } else {
                 if (butao.equals("alterar")) {
-                    t.setEntidade((Entidade) request.getSession().getAttribute("entidade"));
-                    setDados(t, t.getId(), t.getProfissao(), t.getTipo(), t.getQuantidadeVagas(), t.getSalario(), t.getDescricao(), t.getTempoInicio(), t.getTempoFinal());
+                    String id = request.getParameter("id");
+                    Long idLong = Long.parseLong(id);
+                    t = tdao.getById(idLong);
+                    t.setProfissao(request.getParameter("profissao"));
+                    t.setTipo(request.getParameter("tipo").charAt(0));
+                    t.setQuantidadeVagas(Integer.parseInt(request.getParameter("quantidadeVagas")));
+                    t.setSalario(Double.parseDouble(request.getParameter("salario")));
+                    t.setDescricao(request.getParameter("descricao"));
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                    df.setLenient(false);
+                    try {
+                        t.setTempoFinal(df.parse(request.getParameter("fim")));
+                        t.setTempoInicio(df.parse(request.getParameter("inicio")));
+                    } catch (ParseException ex) {
+                        Logger.getLogger(TrabalhoServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
 
                     String str;
                     try {
@@ -94,18 +109,6 @@ public class TrabalhoServlet extends HttpServlet {
                 }
             }
         }
-    }
-
-    private void setDados(Trabalho t, Long id, String profissao, char tipo, int qtdeVagas,
-            Double salario, String descricao, Date inicio, Date fim) {
-        t.setId(id);
-        t.setProfissao(profissao);
-        t.setTipo(tipo);
-        t.setQuantidadeVagas(qtdeVagas);
-        t.setSalario(salario);
-        t.setDescricao(descricao);
-        t.setTempoInicio(inicio);
-        t.setTempoFinal(fim);
     }
 
 }
